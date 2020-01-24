15fc875b28a905ec7f4953aba85acfce22b6fc0c
arita37 <noelkev0@gmail.com>
2020-01-25 02:00:12 +0900

    all

---
+++ viz/run.py
@@ -0,0 +1,49 @@
+"""
+
+
+conda install -c ankurankan pgmpy
+
+#conda create -n env_BNLEARN python=3.6
+#conda activate env_BNLEARN
+
+conda install pytorch
+pgmpy
+numpy
+pandas
+tqdm
+statsmodels
+community
+networkx==1.11
+matplotlib==2.2.3
+
+#conda install spyder
+
+
+
+
+"""
+import bnlearn as bn
+
+
+
+# Example dataframe sprinkler_data.csv can be loaded with: 
+df = bn.import_example()
+# df = pd.read_csv('sprinkler_data.csv')
+
+model = bn.structure_learning(df)
+G = bn.plot(model)
+
+
+
+
+
+import pgmpy
+
+
+import numpy as np
+
+
+
+
+a =1
+
