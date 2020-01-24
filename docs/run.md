7268bf1a399efa067f254db70fa89b136a14d594
arita37 <noelkev0@gmail.com>
2020-01-24 22:10:49 +0900

    update code working

--- docs/run.md
+++ docs/run.md
@@ -1,15 +1,26 @@
 1. Install Debian Dependencies:
 ```
+
+
 sudo apt-get update && sudo apt-get install -y \
     build-essential \
     libssl-dev \
     uuid-dev \
     libgpgme11-dev \
-    squashfs-tools
+    squashfs-tools     
+
+
+
+
 ```
 
 2. Install Go
 ```
+
+Need to remove manually
+/usr/local/go/
+
+
 export VERSION=1.11 OS=linux ARCH=amd64
 cd /tmp
 wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
@@ -17,6 +28,9 @@ sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 echo 'export GOPATH=${HOME}/go' >> ~/.bashrc
 echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc
 source ~/.bashrc
+
+
+
 ```
 
 3. Install Singularity
@@ -34,17 +48,30 @@ go get -u -v github.com/golang/dep/cmd/dep
 
 5. Compile Singular Binary
 ```
+sudo apt-get install uuid-dev
+
+
+
 cd $GOPATH/src/github.com/sylabs/singularity
 ./mconfig
 make -C builddir
 sudo make -C builddir install
+
+
+
 ```
 
 6. Clone code repository
 ```
 cd $GOPATH/src/github.com
+
+
 git clone https://github.com/kurowasan/GraN-DAG.git
 cd GraN-DAG
+
+
+
+
 ```
 
 7. Extract the dataset you want to train on. Update the dataset name in the work.sh if you want to train on different dataset
@@ -60,7 +87,9 @@ gedit start_example.sh
 CODE_PATH="~/go/src/github.com/GraN-DAG"
 EXP_PATH="~/go/src/github.com/GraN-DAG/experiments"
 DATA_PATH="~/go/src/github.com/GraN-DAG/data/data_p10_e10_n1000_GP"
-. ./start_example.sh
+./start_example.sh
+
+
 ```
 
 9. To simulate the baseline results, uncomment the desired algorithm in start_example.sh and rerun 8.
--- start_example.sh
+++ start_example.sh
@@ -2,28 +2,52 @@
 # make sure container.simg is in your working directory
 # make sure singularity is installed on your computer.
 
-CODE_PATH="~/go/src/github.com/GraN-DAG"  # path to the code root
-EXP_PATH="~/go/src/github.com/GraN-DAG/experiments"  # this folder will contain all artifacts saved by the program
-DATA_PATH="~/go/src/github.com/GraN-DAG/data/data_p10_e10_n1000_GP"  # this should contain (data1.npy, DAG1.npy, CPDAG1.npy), (data2.npy, DAG2.npy, CPDAG2.npy), ...
+CODE_PATH="/mnt/hgfs/d/gitdev/_gran_dag"  # path to the code root
+EXP_PATH="/mnt/hgfs/d/gitdev/_gran_dag/experiments"  # this folder will contain all artifacts saved by the program
+DATA_PATH="/mnt/hgfs/d/gitdev/_gran_dag/data/data_p10_e10_n1000_GP"  # this should contain (data1.npy, DAG1.npy, CPDAG1.npy), (data2.npy, DAG2.npy, CPDAG2.npy), ...
+
+
+
+
 MODEL="NonLinGaussANM"  # or NonLinGauss
 DATA_INDEX=1  # Choose which dataset to use. Program will use (data${DATA_INDEX}.npy, DAG${DATA_INDEX}.npy, CPDAG${DATA_INDEX}.npy)
 NUM_VARS=10  # should match the data provided
 
+
+
+
 # Run python program. This is the Augmented Lagrangian procedure, without PNS. (add --pns to the command line to perform PNS)
 # For GraN-DAG
-singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python main.py --exp-path /final_log/ --data-path /dataset/ --#i-dataset ${DATA_INDEX} --model $MODEL --train --to-dag --num-vars ${NUM_VARS} --jac-thresh"
+singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python main.py --exp-path /final_log/ --data-path /dataset/ --i-dataset ${DATA_INDEX} --model $MODEL --train --to-dag --num-vars ${NUM_VARS} --jac-thresh"
+
+
 
 # For cam
-#singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python cam/main.py --exp-path /final_log/ --data-path /dataset/ --#i-dataset ${DATA_INDEX}"
+#singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python cam/main.py --exp-path /final_log/ --data-path /dataset/ --i-dataset ${DATA_INDEX}"
+
+
+
 
 # For dag_gnn
 #singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python dag_gnn/main.py --exp-path /final_log/ --data-path /#dataset/ --i-dataset ${DATA_INDEX}"
 
+
+
+
 # For notears
 #singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python notears/main.py --exp-path /final_log/ --data-path /#dataset/ --i-dataset ${DATA_INDEX}"
+
+
+
 
 # For random_baseline
 #singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && #python random_baseline/main.py --exp-path /final_log/ --data-path /dataset/ --i-dataset ${DATA_INDEX}"
 
+
+
+
 # To use the baselines methods, replace main.py by the right path (e.g. for notears, it is notears/main.py)
 # singularity exec --containall -B $DATA_PATH:/dataset/ -B $EXP_PATH:/final_log/ -B $CODE_PATH:/code/ ./container.simg bash -c "cd /code && python cam/main.py --exp-path /final_log/ --data-path /dataset/ --i-dataset ${DATA_INDEX}"
+
+
+
