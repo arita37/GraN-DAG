38c248a4b2c522901f4dc0786b13d54df61995d6
arita37 <noelkev0@gmail.com>
2020-01-26 16:05:40 +0900

    update code

--- viz/bnlearn_results/generate.py
+++ viz/bnlearn_results/generate.py
@@ -2,6 +2,11 @@ import bnlearn as bnlearn
 import numpy as np
 import pandas as pd
 
+
+
+
+print(pd)
+
 cpdag = pd.DataFrame(np.load("CPDAG1.npy"))
 dag = pd.DataFrame(np.load("DAG1.npy"))
 data = pd.DataFrame(np.load("data1.npy"))
@@ -13,4 +18,53 @@ G = bnlearn.plot(model)
 #model = bnlearn.import_DAG(CPD=False)
 #model_update = bnlearn.parameter_learning(model, dag)
 # = bnlearn.plot(model)
+
+
+
+import os
+
+print(os)
+
+import numpy
+
+
+print(numpy)
+
+
+
+import numpy as np
+
+
+np.cos()
+
+
+
+import matplotlib as mpl
+
+mpl.__version__
+
+
+
+print(mpl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
